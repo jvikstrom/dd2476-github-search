3d1c7dd27e88d9f9e82e0700aed7cc8eb111be3b
Edvin Ardö <ardo@kth.se>
2020-05-10 20:23:20 +0200

    Added search for classes and method calls.

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -9,9 +9,10 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce response-atom (atom nil))
+(defonce index-atom (atom "method"))
 (defonce name-atom (atom nil))
 (defonce return-type-atom (atom nil))
-(defonce return-type-toggle-atom (atom nil))
+(defonce return-type-toggle-atom (atom false))
 
 (defn handler! [response]
   (do (reset! response-atom response)
@@ -30,8 +31,8 @@
      {:match
       {:NAME @name-atom}})})
 (defn search
-  []
-  (POST "http://92.34.13.80:9200/github/_search?size=100"
+  [index]
+  (POST (str "http://92.34.13.80:9200/" index "/_search?size=10000")
         {:handler         handler!
          :error-handler   error-handler
          :params          (create-query)
@@ -55,9 +56,36 @@
   [:div
    [:h1 "GitHub Index"]
    [:form
-    {:on-submit (fn [e] (do (search)
+    {:on-submit (fn [e] (do (search (if index-atom
+                                      "method"
+                                      "class"))
                             (.preventDefault e)))}
-    [:label "Method name: "]
+    [:form
+     [:section
+      [:label {:for "radio-method"}
+       "Method"]
+      [:input {:type     "radio"
+               :id       "radio-method"
+               :name     "index-radio-button"
+               :on-click (fn [] (reset! index-atom "method"))
+               :checked  (= @index-atom "method")}]]
+     [:section
+      [:label {:for "radio-method-call"}
+       "Method call"]
+      [:input {:type     "radio"
+               :id       "radio-method-call"
+               :name     "index-radio-button"
+               :on-click (fn [] (reset! index-atom "method-call"))
+               :checked  (= @index-atom "method-call")}]]
+     [:section
+      [:label {:for "radio-class"}
+       "Class"]
+      [:input {:type     "radio"
+               :id       "radio-class"
+               :name     "index-radio-button"
+               :on-click (fn [] (reset! index-atom "class"))
+               :checked  (= @index-atom "class")}]]]
+    [:label "Name: "]
     [:input {:type      "text"
              :value     @name-atom
              :on-change (fn [val] (reset! name-atom (.-value (.-target val))))}]
@@ -66,34 +94,42 @@
      [:input {:type      "text"
               :value     @return-type-atom
               :on-change (fn [val] (reset! return-type-atom (.-value (.-target val))))
-              :disabled  (not @return-type-toggle-atom)}]
+              :disabled  (and (not @return-type-toggle-atom)
+                              (= @index-atom
+                                 "class"))}]
      [:input {:type     "checkbox"
-              :on-click (fn [] (swap! return-type-toggle-atom (fn [val] (not val))))}]]
+              :on-click (fn [] (swap! return-type-toggle-atom (fn [val] (not val))))
+              :disabled (= @index-atom
+                           "class")}]]
     [:button {:on-click search} "Search"]]
-   (if (= (-> @response-atom
-              :hits
-              :hits)
-          [])
-     [:div "No search results were found."]
-     [:table {:style {:border          "1px solid black"
-                      :border-collapse "collapse"
-                      :width           "100%"}}
-      [:tbody
-       (->> @response-atom
-            :hits
-            :hits
-            (map :_source)
-            (map-indexed (fn [idx hit] [:tr {:key   (str "row-" idx)
-                                             :style (if (even? idx)
-                                                      {:background-color "#dddddd"}
-                                                      {})}
-                                        [:td {:style {
-                                                      :text-align "left"
-                                                      :padding    "8px"}}
-                                         (let [url (if (= (:line hit) 0)
-                                                     (:URL hit)
-                                                     (str (str (:URL hit) "#Lcd ..") (:LINE hit)))]
-                                           [:a {:href url} url])]])))]])])
+   (let [hits (-> @response-atom
+                  :hits
+                  :hits)]
+     (if (= hits
+            [])
+       [:div (str "No search results were found in " (:took @response-atom) " ms.")]
+       [:div
+        (if (some? @response-atom)
+          [:div (str "Found " (count hits) " matching entries in " (:took @response-atom) " ms.")]
+          nil)
+        [:table {:style {:border          "1px solid black"
+                         :border-collapse "collapse"
+                         :width           "100%"}}
+         [:tbody
+          (->> hits
+               (map :_source)
+               (map-indexed (fn [idx hit] [:tr {:key   (str "row-" idx)
+                                                :style (if (even? idx)
+                                                         {:background-color "#dddddd"}
+                                                         {})}
+                                           [:td {:style {
+                                                         :text-align "left"
+                                                         :padding    "8px"}}
+                                            (let [url (if (= (:line hit) 0)
+                                                        (:URL hit)
+                                                        (str (str (:URL hit) "#L") (:LINE hit)))]
+                                              [:a {:href   url
+                                                   :target "_blank"} url])]])))]]]))])
 
 (reagent/render-component [main]
                           (. js/document (getElementById "app")))
